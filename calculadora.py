690e6766b84a7302a89fe8b0ec7a79dd77e5674e
gabriel silva de oliveira pedro <gabrielsop@yahoo.com.br>
2025-10-22 20:58:17 -0300

    implementa a função de subtração

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+x = int(input("Digite o valor de X: "))
+y = int(input("Digite o valor de Y: "))
+
+print (f"valor digitado: {x, y}")
+
+soma = (x + y) * 2 
+print (f"valor da soma multiplicado por 2: {soma}")
+
+sub = (x - y)
+
+print(f"Valor da subtração: {sub}")
