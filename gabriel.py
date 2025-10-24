9ec06544267862e9c3bac5bce403ea56077ca5fd
gabriel silva de oliveira pedro <gabrielsop@yahoo.com.br>
2025-10-23 21:16:07 -0300

    codigo teste

---
+++ gabriel.py
@@ -0,0 +1,7 @@
+x = int(input("digite um valor: "))
+y = int(input("Digite outro valor: "))
+
+print(f"O valor digitado e {x, y}")
+soma = (x+y) 
+soma2 = (x*y)
+print(f"o valor da soma e {soma}.\nO valor da multiplicação e {soma2} ")    
